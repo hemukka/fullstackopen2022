d700d6cbd349430287fd62cb43819afed954f808
hemukka <henri.munukka@gmail.com>
2022-03-18 15:23:19 +0200

    osa 2e

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -53,11 +53,33 @@ const Filter = (props) => {
   )
 }
 
+const Notification = ({message, color='green'}) => {
+  if (message === null) {
+    return null
+  }
+  const notificationStyle = {
+    color: color,
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+  return (
+    <div style={notificationStyle}>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
+  const [messageColor, setMessageColor] = useState('green')
 
   useEffect(() => {
     personService
@@ -79,6 +101,21 @@ const App = () => {
             setPersons(persons.map(p => p.id !== person.id ? p : returnedPerson))
             setNewName('')
             setNewNumber('')
+            setMessage(`${returnedPerson.name} number changed to ${returnedPerson.number}`)
+            setTimeout(() => {
+              setMessage(null)
+            }, 4000)
+          })
+          .catch(error => {
+            setMessage(`Information of ${person.name} has already been removed from server`)
+            setMessageColor('red')
+            setTimeout(() => {
+              setMessage(null)
+              setMessageColor('green')
+            }, 4000)
+            setPersons(persons.filter(p => p.id !== person.id))
+            setNewName('')
+            setNewNumber('')
           })
       }
     } else {
@@ -91,16 +128,35 @@ const App = () => {
           setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
+          setMessage(`Added ${returnedPerson.name}`)
+            setTimeout(() => {
+              setMessage(null)
+            }, 4000)
         })
     }
   }
 
   const removePerson = (id) => {
-    if (window.confirm(`Delete ${persons.find(p => p.id === id).name} ?`)) {
+    const removeName = persons.find(p => p.id === id).name
+    if (window.confirm(`Delete ${removeName} ?`)) {
       personService
       .remove(id)
-      .then(setPersons(persons.filter(p => p.id !== id))
-      )
+      .then(() => {
+        setPersons(persons.filter(p => p.id !== id))
+        setMessage(`Deleted ${removeName}`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 4000)
+      })
+      .catch(error => {
+        setMessage(`Information of ${removeName} has already been removed from server`)
+        setMessageColor('orange')
+        setTimeout(() => {
+          setMessage(null)
+          setMessageColor('green')
+        }, 4000)
+        setPersons(persons.filter(p => p.id !== id))
+      })
     }
   }
 
@@ -112,15 +168,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} color={messageColor} />
       <Filter filter={filter} handleChange={(event) => setFilter(event.target.value)} />
-
       <h3>add a new</h3>
       <PersonForm handleSubmit={(event) => addPerson(event)}
         nameValue={newName}
         handleNameChange={(event) => setNewName(event.target.value)}
         numberValue={newNumber}
         handleNumberChange={(event) => setNewNumber(event.target.value)} />
-
       <h3>Numbers</h3>
       <Persons
         persons={personsToShow}
